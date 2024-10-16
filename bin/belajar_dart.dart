c3cc6ae0d8cf6109969db3e1c4f0cb6a757762f8
Faishal <ishal.2401@gmail.com>
2024-10-16 16:50:47 +0700

    Update

--- bin/belajar_dart.dart
+++ bin/belajar_dart.dart
@@ -3,9 +3,15 @@ import 'dart:io';
 import 'package:belajar_dart/belajar_dart.dart';
 
 void main() {
+  print("====================================");
   hitung();
+  print("====================================");
+  greeting(nama:'Faishal', umur: 20);
+  greeting(nama: 'Faishal');
+  print("====================================");
 }
 
+// operasi konversi suhu
 void hitung() {
   stdout.write('Masukkan suhu dalam Fahrenheit: ');
   var fahrenheit = num.parse(stdin.readLineSync()!);
@@ -17,4 +23,15 @@ void hitung() {
   print('$fahrenheit derajat Fahrenheit = $celsius derajat celsius');
   print('$fahrenheit derajat Fahrenheit = $kelvin derajat kelvin');
   print('$fahrenheit derajat Fahrenheit = $reamur derajat reamur');
+}
+
+// opsional parameter
+void greeting({String? nama, num? umur}) {
+  if (nama != null && umur != null) {
+    print('Halo, nama saya $nama. Umur saya $umur tahun.');
+  } else if (nama != null) {
+    print('Halo, nama saya $nama.');
+  } else {
+    print('Halo, selamat datang!');
+  }
 }
